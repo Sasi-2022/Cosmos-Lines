d112a910feed7d863c2b88e06ae880e74a4c9d8a
Abisha-SM <abisha1720997@gmail.com>
2024-06-13 22:56:32 +0530

    -Levelsload

--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -43,14 +43,16 @@ namespace Connect.Core
 
         #region GAME_VARIABLES
 
-        [HideInInspector]
+        
         public int CurrentStage;
 
-        [HideInInspector]
+        
         public int CurrentLevel;
 
-        [HideInInspector]
+        
         public string StageName;
+
+        public LevelData[] level;
 
         public bool IsLevelUnlocked(int level)
         {
@@ -105,14 +107,168 @@ namespace Connect.Core
 
         public LevelData GetLevel()
         {
+            
+            
             string levelName = "Level" + CurrentStage.ToString() + CurrentLevel.ToString();
 
-            if(Levels.ContainsKey(levelName))
+            if (CurrentLevel == 1&&CurrentStage==1)
+            {
+                DefaultLevel = level[0];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 1)
+            {
+                DefaultLevel = level[1];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 1)
+            {
+                DefaultLevel = level[2];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 1)
+            {
+                DefaultLevel = level[3];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 1)
+            {
+                DefaultLevel = level[4];
+            }
+            if (CurrentLevel == 1 && CurrentStage == 2)
+            {
+                DefaultLevel = level[5];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 2)
+            {
+                DefaultLevel = level[6];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 2)
+            {
+                DefaultLevel = level[7];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 2)
+            {
+                DefaultLevel = level[8];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 2)
+            {
+                DefaultLevel = level[9];
+            }
+
+            if (CurrentLevel == 1 && CurrentStage == 3)
+            {
+                DefaultLevel = level[10];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 3)
+            {
+                DefaultLevel = level[11];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 3)
+            {
+                DefaultLevel = level[12];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 3)
+            {
+                DefaultLevel = level[13];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 3)
+            {
+                DefaultLevel = level[14];
+            }
+
+            if (CurrentLevel == 1 && CurrentStage == 4)
+            {
+                DefaultLevel = level[15];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 4)
+            {
+                DefaultLevel = level[16];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 4)
+            {
+                DefaultLevel = level[17];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 4)
+            {
+                DefaultLevel = level[18];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 4)
+            {
+                DefaultLevel = level[19];
+            }
+
+
+            if (CurrentLevel == 1 && CurrentStage == 5)
+            {
+                DefaultLevel = level[20];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 5)
+            {
+                DefaultLevel = level[21];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 5)
+            {
+                DefaultLevel = level[22];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 5)
+            {
+                DefaultLevel = level[23];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 5)
+            {
+                DefaultLevel = level[24];
+            }
+
+            if (CurrentLevel == 1 && CurrentStage == 6)
+            {
+                DefaultLevel = level[25];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 6)
+            {
+                DefaultLevel = level[26];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 6)
+            {
+                DefaultLevel = level[27];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 6)
+            {
+                DefaultLevel = level[28];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 6)
+            {
+                DefaultLevel = level[29];
+            }
+
+            if (CurrentLevel == 1 && CurrentStage == 7)
+            {
+                DefaultLevel = level[30];
+            }
+            if (CurrentLevel == 2 && CurrentStage == 7)
+            {
+                DefaultLevel = level[31];
+            }
+            if (CurrentLevel == 3 && CurrentStage == 7)
+            {
+                DefaultLevel = level[32];
+            }
+            if (CurrentLevel == 4 && CurrentStage == 7)
+            {
+                DefaultLevel = level[33];
+            }
+            if (CurrentLevel == 5 && CurrentStage == 7)
+            {
+                DefaultLevel = level[34];
+            }
+
+            if (Levels.ContainsKey(levelName))
             {
                 return Levels[levelName];
             }
 
             return DefaultLevel;
+        }
+
+        public void Loadlevel()
+        {
+            
         }
 
         #endregion
